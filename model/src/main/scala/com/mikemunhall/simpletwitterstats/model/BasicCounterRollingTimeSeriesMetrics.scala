a0f9fd6576811a13384cf9ed37ca2a8772eef6dd
Mike Munhall <munhall.mike@gmail.com>
2016-11-02 12:08:22 -0600

    Clarified code comment.

--- model/src/main/scala/com/mikemunhall/simpletwitterstats/model/BasicCounterRollingTimeSeriesMetrics.scala
+++ model/src/main/scala/com/mikemunhall/simpletwitterstats/model/BasicCounterRollingTimeSeriesMetrics.scala
@@ -11,6 +11,7 @@ import java.time.LocalDateTime
   */
 class BasicCounterRollingTimeSeriesMetrics[`scala.Long`](label: String, default: () => scala.Long) extends RollingTimeSeriesMetrics(label, default) {
   def add(timestamp: LocalDateTime, obj: Any) = {
+    // TODO: Understand why Long is cast to String. scala.Long is not cast.
     if (obj.asInstanceOf[Boolean]) values(timestamp.getHour)(timestamp.getMinute)(timestamp.getSecond) += 1
   }
 }
--- model/src/main/scala/com/mikemunhall/simpletwitterstats/model/ListLenCounterRollingTimeSeriesMetrics.scala
+++ model/src/main/scala/com/mikemunhall/simpletwitterstats/model/ListLenCounterRollingTimeSeriesMetrics.scala
@@ -11,6 +11,7 @@ import java.time.LocalDateTime
   */
 class ListLenCounterRollingTimeSeriesMetrics[`scala.Long`](label: String, default: () => scala.Long) extends RollingTimeSeriesMetrics(label, default) {
   def add(timestamp: LocalDateTime, obj: Any) = {
+    // TODO: Understand why Long is cast to String. scala.Long is not cast.
     if (obj.asInstanceOf[List[String]].size > 0) values(timestamp.getHour)(timestamp.getMinute)(timestamp.getSecond) += 1
   }
 }
